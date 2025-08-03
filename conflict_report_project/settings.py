ba4acc37f1c3fafe1908f4725bf6da4854164066
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-08-03 19:14:39 +0100

    Added secret key and set is an environment variable in env.py file. Modified setting.py to retrieve secret key

--- conflict_report_project/settings.py
+++ conflict_report_project/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Retrieval of the secret key from environment variable
+SECRET_KEY = os.environ.get("SECRET_KEY")

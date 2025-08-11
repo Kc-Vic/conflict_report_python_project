46b2a4e38ac3b2dd17157c3d4142b253690a4e1b
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-08-11 22:24:03 +0100

    Create fixture and added state.json file to store list of states

--- civ_intel/models.py
+++ civ_intel/models.py
@@ -10,44 +10,15 @@ STATUS = (
 
 # Create your models here.
 class State(models.Model):
-    state_choices = [
-        ('AB', 'Abia'),
-        ('AD', 'Adamawa'),
-        ('AK', 'Akwa Ibom'),
-        ('AN', 'Anambra'),
-        ('BA', 'Bauchi'),
-        ('BY', 'Bayelsa'),
-        ('BE', 'Benue'),
-        ('BO', 'Borno'),
-        ('CR', 'Cross River'),
-        ('DE', 'Delta'),
-        ('EB', 'Ebonyi'),
-        ('ED', 'Edo'),
-        ('EK', 'Ekiti'),
-        ('EN', 'Enugu'),
-        ('FC', 'Federal Capital Territory'),
-        ('GO', 'Gombe'),
-        ('IM', 'Imo'),
-        ('JI', 'Jigawa'),
-        ('KD', 'Kaduna'),
-        ('KN', 'Kano'),
-        ('KT', 'Katsina'),
-        ('KE', 'Kebbi'),
-        ('KO', 'Kogi'),
-        ('KW', 'Kwara'),
-        ('LA', 'Lagos'),
-        ('NA', 'Nasarawa'),
-        ('NI', 'Niger'),
-        ('OG', 'Ogun'),
-        ('ON', 'Ondo'),
-        ('OS', 'Osun'),
-        ('OY', 'Oyo'),
-        ('PL', 'Plateau'),
-        ('RI', 'Rivers'),
-        ('SO', 'Sokoto'),
-        ('TA', 'Taraba'),
-        ('YO', 'Yobe'),
-    ]
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=10, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "States"
+        
+    def __str__(self):
+        return self.name
 
 
 class Report(models.Model):

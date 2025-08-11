6431a3e7b1d27d4a97d9c8d3e6d3a890bf60d510
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-08-12 00:21:19 +0100

    Installed summernote package and modified admin dashboard

--- civ_intel/admin.py
+++ civ_intel/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from civ_intel.models import Report, Comment
 from civ_intel.models import State
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Report)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Report)
 admin.site.register(State)
 admin.site.register(Comment)

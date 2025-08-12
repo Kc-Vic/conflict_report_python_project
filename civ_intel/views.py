54ed6b9db14506552b1a2c6364b2ca5bb8ce5b1e
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-08-12 02:03:26 +0100

    create base.html and index.html files under templates folder. Added css style to static folder

--- civ_intel/views.py
+++ civ_intel/views.py
@@ -5,4 +5,5 @@ from .models import Report
 # Create your views here.
 class ReportFeedView(generic.ListView):
    queryset = Report.objects.all().order_by('-created_at')
-   template_name = 'civ_intel/report_list.html'
+   template_name = 'civ_intel/index.html'
+   paginate_by = 10

bf0c778dec8fdec9c950449b170cf7b279714cc6
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-08-14 00:41:05 +0100

    installed django allauth and created HTML code for login and signup page

--- conflict_report_project/urls.py
+++ conflict_report_project/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("civ_intel.urls"), name="civ_intel-urls"),
-    
+    path("accounts/", include("allauth.urls")),
     
 ]

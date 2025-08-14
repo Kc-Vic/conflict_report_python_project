8c9a3eb3289acdd6b8d2fe2c2b9a561ffe11ac11
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-08-14 05:35:59 +0100

    created html code for adding comment and included function in view.py

--- civ_intel/urls.py
+++ civ_intel/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.ReportAboutView.as_view(), name='home'),
     path('report_list/', views.ReportFeedView.as_view(), name='feed'),
+    path('add_comment/', views.add_comment, name='add_comment'),
     path('<slug:slug>/', views.report_detail, name='report_detail'),
+    
 ]
--- civ_intel/views.py
+++ civ_intel/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Report
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -17,6 +18,7 @@ def report_detail(request, slug):
    report = get_object_or_404(queryset, slug=slug)
    comments = report.comments.all().order_by('-created_at')
    comment_count = report.comments.count()
+   comment_form = CommentForm()
 
 
    return render(
@@ -24,6 +26,23 @@ def report_detail(request, slug):
       {
          'report': report,
          'comments': comments,
-         'comment_count': comment_count
+         'comment_count': comment_count,
+         'comment_form': comment_form,
       },
    )
+
+def add_comment(request):
+   if request.method == 'POST':
+      comment_form = CommentForm(request.POST)
+      if comment_form.is_valid():
+         comment = comment_form.save(commit=False)
+         comment.author = request.user
+         report_slug = request.POST.get('report_slug')
+         report = get_object_or_404(Report, slug=report_slug)
+         comment.report = report
+         comment.save()
+         return redirect('report_detail', slug=report.slug)
+   else:
+      comment_form = CommentForm()
+
+   return render(request, 'civ_intel/add_comment.html', {'comment_form': comment_form})
